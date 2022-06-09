3150d888c7eb29dc1f47d7e9ef7e43ee8042cd4b
Holy Crusader <holy.tanukis@gmail.com>
2022-06-09 23:27:55 +0200

    Create a page for /dashboard

--- src/app/views.py
+++ src/app/views.py
@@ -3,7 +3,6 @@ from .models import Card
 
 
 def index(request):
-    print(Card.objects.all())
     context = {
         'connected': False,
         'presentations': Card.objects.all()
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def guild_list(request):
-    return HttpResponse(f"List of the guilds")
+    return render(request, "dashboard/index.html")
 
 def dashboard(request, guild_id):
     return HttpResponse(f"Dashboard of the guild {guild_id}")
